fd1c91f34162f1bf12d5ef4da0019d606bd0865f
Andrew Hickman <andrew.hickman1@sky.com>
2022-06-05 21:06:37 +0100

    Public API

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::{fmt, path::Path};
+
+use prost_types::FileDescriptorSet;
+
+pub fn compile(
+    _files: impl IntoIterator<Item = impl AsRef<Path>>,
+    _includes: impl IntoIterator<Item = impl AsRef<Path>>,
+) -> Result<FileDescriptorSet, Error> {
+    todo!()
+}
+
+pub struct Error {
+    msg: String,
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.msg.fmt(f)
     }
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.msg.fmt(f)
+    }
+}
+
+impl std::error::Error for Error {}
