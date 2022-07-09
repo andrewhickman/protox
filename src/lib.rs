a85f1ab64bbb32925d19bd8f27be18d6c1eb6126
Andrew Hickman <andrew.hickman1@sky.com>
2022-07-09 03:07:52 +0100

    Add doctest

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,84 @@ pub use self::files::{File, FileImportResolver, ImportResolver};
 /// # Ok(())
 /// # }
 /// ```
+///
+/// # Examples
+///
+/// ```
+/// # use std::fs;
+/// # use prost_types::{
+/// #    DescriptorProto, FieldDescriptorProto, field_descriptor_proto::{Label, Type}, FileDescriptorSet, FileDescriptorProto,
+/// #    SourceCodeInfo, source_code_info::Location
+/// # };
+/// # use protox::compile;
+/// # let tempdir = assert_fs::TempDir::new().unwrap();
+/// # std::env::set_current_dir(tempdir).unwrap();
+/// #
+/// fs::write("bar.proto", "
+///     message Bar { }
+/// ").unwrap();
+/// fs::write("root.proto", "
+///     import 'bar.proto';
+///
+///     message Foo {
+///         optional Bar bar = 1;
+///     }
+/// ").unwrap();
+///
+/// assert_eq!(compile(&["root.proto"], &["."]).unwrap(), FileDescriptorSet {
+///     file: vec![
+///         FileDescriptorProto {
+///             name: Some("bar.proto".to_owned()),
+///             message_type: vec![DescriptorProto {
+///                 name: Some("Bar".to_owned()),
+///                 ..Default::default()
+///             }],
+///             source_code_info: Some(SourceCodeInfo {
+///                 /* ... */
+/// #               location: vec![
+/// #                   Location { path: vec![], span: vec![1, 4, 19], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0], span: vec![1, 4, 19], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0, 1], span: vec![1, 12, 15], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #               ],
+///             }),
+///             ..Default::default()
+///         },
+///         FileDescriptorProto {
+///             name: Some("root.proto".to_owned()),
+///             dependency: vec!["bar.proto".to_owned()],
+///             message_type: vec![DescriptorProto {
+///                 name: Some("Foo".to_owned()),
+///                 field: vec![FieldDescriptorProto {
+///                     name: Some("bar".to_owned()),
+///                     number: Some(1),
+///                     label: Some(Label::Optional as _),
+///                     r#type: Some(Type::Message as _),
+///                     type_name: Some(".Bar".to_owned()),
+///                     json_name: Some("bar".to_owned()),
+///                     ..Default::default()
+///                 }],
+///                 ..Default::default()
+///             }],
+///             source_code_info: Some(SourceCodeInfo {
+///                 /* ... */
+/// #               location: vec![
+/// #                   Location { path: vec![3, 0], span: vec![1, 4, 23], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0, 1], span: vec![3, 12, 15], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![], span: vec![4, 4, 5, 5], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0], span: vec![4, 4, 5, 5], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0, 2, 0], span: vec![4, 8, 29], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0, 2, 0, 1], span: vec![4, 21, 24], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0, 2, 0, 3], span: vec![4, 27, 28], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0, 2, 0, 4], span: vec![4, 8, 16], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #                   Location { path: vec![4, 0, 2, 0, 6], span: vec![4, 17, 20], leading_comments: None, trailing_comments: None, leading_detached_comments: vec![] },
+/// #               ],
+///             }),
+///             ..Default::default()
+///         },
+///     ],
+///     ..Default::default()
+/// });
+/// ```
 pub fn compile(
     files: impl IntoIterator<Item = impl AsRef<Path>>,
     includes: impl IntoIterator<Item = impl AsRef<Path>>,
