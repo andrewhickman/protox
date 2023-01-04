1af90c32aeaac3bd8ca5b70d96de88b7a90f1ae5
Andrew Hickman <andrew.hickman1@sky.com>
2023-01-04 21:55:22 +0000

    Improve CLI interface

--- protox/src/main.rs
+++ protox/src/main.rs
@@ -2,13 +2,14 @@ use std::{fs, path::PathBuf};
 
 use clap::Parser;
 use miette::Result;
-use prost::Message;
-use protox::compile;
+use protox::Compiler;
 
 #[derive(Debug, Parser)]
 pub struct Args {
+    /// The source file(s) to compile
     #[clap(value_name = "PROTO_FILES", required = true, value_parser)]
     files: Vec<PathBuf>,
+    /// The directory in which to search for imports.
     #[clap(
         short = 'I',
         long = "include",
@@ -18,6 +19,7 @@ pub struct Args {
         value_parser
     )]
     includes: Vec<PathBuf>,
+    /// The output path to write a file descriptor set to.
     #[clap(
         short = 'o',
         long = "output",
@@ -26,16 +28,27 @@ pub struct Args {
         value_parser
     )]
     output: Option<PathBuf>,
+    /// If set, includes source code information in the output file descriptor set.
+    #[clap(long, visible_alias = "include_source_info")]
+    include_source_info: bool,
+    /// If set, all dependencies of the input files are output, so that the file descriptor set is self-contained.
+    #[clap(long, visible_alias = "include_imports")]
+    include_imports: bool,
 }
 
 pub fn main() -> Result<()> {
     miette::set_panic_hook();
 
     let args = Args::parse();
-    let files = compile(args.files, args.includes)?;
-    // TODO include_source_info flag, include_imports
+    let mut compiler = Compiler::new(args.includes)?;
+    compiler.include_imports(args.include_imports);
+    compiler.include_source_info(args.include_source_info);
+    for file in args.files {
+        compiler.add_file(file)?;
+    }
     if let Some(output) = args.output {
-        fs::write(output, files.encode_to_vec()).map_err(|err| miette::miette!(err))?;
+        fs::write(output, compiler.encode_file_descriptor_set())
+            .map_err(|err| miette::miette!(err))?;
     }
     Ok(())
 }
