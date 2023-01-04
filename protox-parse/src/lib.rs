e1724e9c3ba66d2c753037d5607f66e4742e7ccb
Andrew Hickman <andrew.hickman1@sky.com>
2023-01-04 22:40:33 +0000

    (cargo-release) version 0.2.0

--- protox-parse/src/lib.rs
+++ protox-parse/src/lib.rs
@@ -3,7 +3,7 @@
 //! See the documentation for [`parse()`] for details.
 #![warn(missing_debug_implementations, missing_docs)]
 #![deny(unsafe_code)]
-#![doc(html_root_url = "https://docs.rs/protox-parse/0.1.0/")]
+#![doc(html_root_url = "https://docs.rs/protox-parse/0.2.0/")]
 
 use std::fmt;
 
