34c2bc86b0c4a6ab7407ff7da772151d5f121665
tottoto <tottotodev@gmail.com>
2024-09-27 05:44:20 +0900

    Use tonic-build compile_fds to build file descriptor set (#87)

--- protox/src/lib.rs
+++ protox/src/lib.rs
@@ -30,15 +30,9 @@
 //!
 //! let file_descriptors = protox::compile(["root.proto"], ["."]).unwrap();
 //!
-//! let file_descriptor_path = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR not set"))
-//!     .join("file_descriptor_set.bin");
-//! fs::write(&file_descriptor_path, file_descriptors.encode_to_vec()).unwrap();
-//!
 //! tonic_build::configure()
 //!     .build_server(true)
-//!     .file_descriptor_set_path(&file_descriptor_path)
-//!     .skip_protoc_run()
-//!     .compile(&["root.proto"], &["."])
+//!     .compile_fds(file_descriptors)
 //!     .unwrap();
 //! ```
 //!
